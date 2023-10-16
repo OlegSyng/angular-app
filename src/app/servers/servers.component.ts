d895512db226478ce164706d823ad0f7eb49ce28
Oleg Syng <oleg@gloobus.it>
2023-10-16 12:44:06 +0300

    complete intro in data binding and directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+class ClickLogItem {
+  constructor(public clickCount: number, public timestamp: Date) {}
+}
+
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  username = '';
+  serverName= 'TestServer';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
+  showSecret = false;
+  clickLog: ClickLogItem[] = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onCreateServer() { 
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+  }
+
+  onClickResetUsername() {
+    this.username = '';
+  }
+
+  onClickShowDetails() {
+    this.showSecret = !this.showSecret;
+    this.clickLog.push(new ClickLogItem(this.clickLog.length + 1, new Date()));
+  }
+
+  showDate(clickLogItem: ClickLogItem) {
+    return clickLogItem.timestamp.toLocaleTimeString();
+  }
 
 }
